08db8af1628182c1acc3ae4a4c2cb3d5e32477e1
Abhiram N <narayananabhiram@gmail.com>
2024-02-28 00:14:57 +0530

    modified

--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-        .delete(`https://epicbooks-mern-abhiram-n.onrender.com/${id}`)
+        .delete(`https://epicbooks-mern-abhiram-n.onrender.com/books/${id}`)
         .then(()=>{
           setLoading(false);
           enqueueSnackbar("Book Deleted successfully", { variant: 'success' });
--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -15,7 +15,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-        .get(`http://localhost:5555/books/${id}`)
+        .get(`https://epicbooks-mern-abhiram-n.onrender.com/books/${id}`)
         .then((response)=>{
           setBook(response.data);
           setLoading(false);
